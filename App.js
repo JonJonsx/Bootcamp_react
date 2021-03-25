2f793c3e170e22945b7dfa6d5391aaf4075536e0
JonJonsx <jonatan.souza@bandtec.com.br>
2021-03-25 00:59:50 -0300

    Adicionando cores ao pokemons

---
+++ App.js
@@ -0,0 +1,13 @@
+var container = document.getElementById("app")
+import Card from './components/'
+
+const App = props => (
+  <React.Fragment>
+    <div>
+    </div>
+    {/* <h1>Vote no seu preferido</h1>
+    <Contador titulo="Charmander" />
+    <Contador titulo="Bulbassaur" />
+    <Contador titulo="Squirtle" /> */}
+  </React.Fragment>
+)
--- index.js
+++ index.js
@@ -2,51 +2,76 @@ var container = document.getElementById("app")
 
 
 
-const Card = () =>{
-  <React.Fragment>
-    
-  </React.Fragment>
-}
+const Botton = (props) =>{
 
-const Container = () => {
-
-}
-
-const Botao = (props) =>{
-  <button>{ texto }</button>
-}
-
-
-const Contador = props => {
-
-  const [numero,setNumero] = React.useState(0);
-
-  function somar() {
-    setNumero(numero+1);
-    console.log("somei e ficou =" + numero)
+  const cardStyle = {
+    backgroundColor : props.cor || '#f00',
+    borderColor: props.cor || '#f00',
   }
 
+  return(
+    <button className="Button" style={cardStyle} onClick={props.opcao}>{props.objetivo}</button>
+  )
+}
+
+
+const Card = (props) => {
+
+  const cardStyle = {
+    backgroundColor : props.cor || '#f00',
+    borderColor: props.cor || '#f00',
+  }
+
+  const [numero, setNumero] = React.useState(0);
+
+  function somar() {
+    setNumero(numero + 1);
+    console.log("somei e ficou =" + numero)
+  }
+  
   function subtrair() {
-    setNumero(numero-1)
+    setNumero(numero - 1)
     console.log("subtrai e ficou =" + numero)
   }
 
   return (
-    <React.Fragment>
-      <h1>{props.titulo}</h1>
-      <h2>{numero}</h2>
-      <button onClick={somar} >+</button>
-      <button onClick={subtrair} >-</button>
-    </React.Fragment>
+    <div className="Card" style={cardStyle}>
+      <div clasName="title">
+        <h4>{props.nome}</h4>
+      </div>
+      <div className="sprite">
+        <img src="" class="" alt=""/>
+      </div>
+      <div className="votos">
+        <h3>Votos: {numero}</h3>
+      </div>
+      <div className="buttons">
+        <Botton opcao={somar} cor={props.botao1} objetivo="+"/>
+        <Botton opcao={subtrair} cor={props.botao2} objetivo="-"/>
+      </div>
+    </div>
   )
 }
 
+const Container = () => {
+  return (
+    <div className="Container">
+      <h1>Vote no seu pokemon favorito</h1>
+      <div className="Cards">
+        <Card nome="Charmander" cor="#FF7675" botao1="#D63031" botao2="#FAB1A0"></Card>
+        <Card nome="Bulbassaur" cor="#55EFC4" botao1="#00B894" botao2="#00CEC9"></Card>
+        <Card nome="Squirtle" cor="#74B9FF" botao1="#0984E3" botao2="#54A0FF"></Card>
+      </div>
+    </div>
+  )
+}
+
+{/* <Contador titulo="Charmander" />
+      <Contador titulo="Bulbassaur" />
+      <Contador titulo="Squirtle" /> */}
 const App = props => (
   <React.Fragment>
-    {/* <h1>Vote no seu preferido</h1>
-    <Contador titulo="Charmander" />
-    <Contador titulo="Bulbassaur" />
-    <Contador titulo="Squirtle" /> */}
+    <Container />
   </React.Fragment>
 )
 
