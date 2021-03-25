b269ca81cecc420399d77bf48f3867003314305a
JonJonsx <jonatan.souza@bandtec.com.br>
2021-03-24 23:18:56 -0300

    Adicionando componentes

--- index.js
+++ index.js
@@ -1,16 +1,54 @@
 var container = document.getElementById("app")
-// var titulo = document.createElement("h1")
-// titulo.innerHTML = "Título inserido po JavaScripto :O \O/"
-// container.appendChild(titulo)
 
-function App(props){
-  return (
-    <React.Fragment>
-      <h1>Eu vim do RecatO</h1>
-      <h2>E eu sou um parágrafo</h2>
-    </React.Fragment>
-  );
+
+
+const Card = () =>{
+  <React.Fragment>
+    
+  </React.Fragment>
 }
 
-ReactDOM.render(React.createElement(App,props,null), container);
+const Container = () => {
+
+}
+
+const Botao = (props) =>{
+  <button>{ texto }</button>
+}
+
+
+const Contador = props => {
+
+  const [numero,setNumero] = React.useState(0);
+
+  function somar() {
+    setNumero(numero+1);
+    console.log("somei e ficou =" + numero)
+  }
+
+  function subtrair() {
+    setNumero(numero-1)
+    console.log("subtrai e ficou =" + numero)
+  }
+
+  return (
+    <React.Fragment>
+      <h1>{props.titulo}</h1>
+      <h2>{numero}</h2>
+      <button onClick={somar} >+</button>
+      <button onClick={subtrair} >-</button>
+    </React.Fragment>
+  )
+}
+
+const App = props => (
+  <React.Fragment>
+    {/* <h1>Vote no seu preferido</h1>
+    <Contador titulo="Charmander" />
+    <Contador titulo="Bulbassaur" />
+    <Contador titulo="Squirtle" /> */}
+  </React.Fragment>
+)
+
+ReactDOM.render(React.createElement(App, { titulo: "Eu vim de props" }, null), container);
 //ReactDOM.render(App(), container);
